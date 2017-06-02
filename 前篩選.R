aa1b82b3cbac5da5000e903967dc40a3548d1d72
anohito <windthunder310@gmail.com>
2017-06-02 08:10:19 +0800

    嘗試修正寫法

--- 前篩選.R
+++ 前篩選.R
@@ -5,14 +5,19 @@ contents = data$content
 rm(data)
 
 # clear data
-# clear html tag
-contents = gsub("<.*?>", " ", contents)
-# 清除 wwwww 這個特殊詞
-contents = gsub("w{2,}", "", contents, ignore.case = TRUE)
-# 清除 xd 這個特殊詞
-contents = gsub("xd+", "", contents, ignore.case = TRUE)
-# 清除從頭到尾都沒有中文的內容
-contents = gsub("^[^\u4E00-\u9FA5]*$", "", contents)
+lapply(contents, function(x){
+  # clear html tag
+  x = gsub("<.*?>", " ", x)
+  # 清除 wwwww 這個特殊詞
+  x = gsub("w{2,}", "", x, ignore.case = TRUE)
+  # 清除 xd 這個特殊詞
+  x = gsub("xd+", "", x, ignore.case = TRUE)
+  # 排除escape掉的html符號 (&nbsp;之類)
+  gsub("&.*?;", "", x)
+  # 清除從頭到尾都沒有中文的內容
+  x = gsub("^[^\u4E00-\u9FA5]*$", "", x)
+})
+
 # 清理掉被篩選掉完全為空的條目
 contents = contents[contents!=""]
 
--- 前篩選資料分析.R
+++ 前篩選資料分析.R
@@ -33,20 +33,20 @@ seg_results = segment(contents_without_eng, seg)
 
 # 把英文部份塞回去
 q = list()
-
 for(index in 1:length(seg_results)) {
   concat = c(seg_results[[index]], eng_cut[[index]][,1])
   concat = concat[!grepl("^\\d+$", concat)] # 排除完全是數字的部份
+  concat = concat[!grepl("^[a-zA-Z]$", concat)] # 排除只有一個英文字的類型(如果這作法排除過頭 應該要修正的是詞庫)
   q[[index]] = concat
 }
-
 seg_results = q
 
 # TODO: 將場次名取代成EVENTNAME這個關鍵字 解決場次名混亂問題
 # CHECK: 或許非必要？ 或許把他統一成ff和cwt兩個字而非全部一樣的EVENTNAME？
-q = lapply(seg_results, function(x){
+seg_results = lapply(seg_results, function(x){
   x = gsub('^FF\\d*$', 'EVENTNAME', x, ignore.case = T)
   x = gsub('^CWT(T|K)*\\d*$', 'EVENTNAME', x, ignore.case = T)
 })
 
+# TODO: 同義詞整合
 
